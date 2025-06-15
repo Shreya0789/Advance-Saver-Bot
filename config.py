4f8962d8902aad0a10efa493d790f3000b1d4cf2
ALEX <54197173+kingrajashish@users.noreply.github.com>
2025-06-15 13:52:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "22581733"))
-API_HASH = getenv("API_HASH", "1db7bdcf908100cc641c6a5276765c3d")
-BOT_TOKEN = getenv("BOT_TOKEN", "7913515675:AAH8nVjeNIlHL6PiuUcw3nnPrQe2d7IvubA")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "6530997270").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "-1002487777733")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002487777733"))
+API_ID = int(getenv("API_ID", "29538829"))
+API_HASH = getenv("API_HASH", "0098baaaa11b512a0a8b8eaf33a4d91a")
+BOT_TOKEN = getenv("BOT_TOKEN", "7884852517:AAFdZZXVDUosP2Q_Ov-Jz_zuy--VlRc14YQ")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "1118716436").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://kingrajashish43211:rOYviMdPMPfCvSlR@cluster0.j3o3sbf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002552274394")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002552274394"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
